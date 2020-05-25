52dabef42b106218724c3a3a3a6f0eb9aab9a6c6
riiking <richard.daseking@gmail.com>
2020-05-25 17:25:51 +0200

    test

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ module.exports.loop = function() {
   //activat energyManagement
   energyManagement.run();
 
-  for(name in _.filter(Game.rooms, e => e.memory.blockedForTransfer != undefined)) {
-      Game.rooms[name].memory.blockedForTransfer.forEach(e => Game.getObjectById(e).room.visual.text('❎',Game.getObjectById(e).pos.x,Game.getObjectById(e).pos.y));
-  }
+  // for(name in _.filter(Game.rooms, e => e.memory.blockedForTransfer != undefined)) {
+  //     Game.rooms[name].memory.blockedForTransfer.forEach(e => Game.getObjectById(e).room.visual.text('❎',Game.getObjectById(e).pos.x,Game.getObjectById(e).pos.y));
+  // }
   // Game.rooms['E11S27'].memory.blockedForTransfer.forEach(e => Game.getObjectById(e).room.visual.text('❎',Game.getObjectById(e).pos.x,Game.getObjectById(e).pos.y));
   // Game.rooms['E12S27'].memory.blockedForTransfer.forEach(e => Game.getObjectById(e).room.visual.text('❎',Game.getObjectById(e).pos.x,Game.getObjectById(e).pos.y));
 
